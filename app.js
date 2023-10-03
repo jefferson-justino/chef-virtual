fffc6fcf1da9fd610639b2dcb816241edc6cbb48
jefferson-justino <115052745+jefferson-justino@users.noreply.github.com>
2023-10-03 15:44:30 -0300

    att

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const routers = require('./rotas/index')
+const cors = require('cors')
 const app= express()
 const {sequelize} = require('./models/index')
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/',routers)
 
 sequelize.sync().then(()=>{
     console.log('Conectado patrão!')
 })
-app.listen(3000,()=>{
+app.listen(3001,()=>{
     console.log('server onfire!')
-})
+})  
